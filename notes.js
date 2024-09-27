8eca52857fe36ecb762df03c7f4b74bee0ff2e64
agnesl10 <agnesl10@nycstudents.net>
2024-09-27 12:12:53 -0400

    werwer

--- notes.js
+++ notes.js
@@ -3,6 +3,8 @@ let variable; // declaring variable
 variable = "variable"; //defines variable
 let number = 7; //one line of code for ^^
 let string = "Hello."; // strong data type
+let truthy = true; //booleans
+let falsey = false; //booleans
 let brigString = "hi i'm a larger string"; //camel case: longer name
 
 //ways to declare variables/
@@ -10,6 +12,19 @@ var variable1 = "var"; // we don't do this
 let variable2; //we can define later or REDEFINE
 variable = "nope";
 console.log(variable); // should display the later line of code of the definition of variable
-const constant = "constant"; //cannot redefine, you will get error msg.
+const constant = "constant"; //cannot redefine, you will get error msg. use this 95% of the time.
+
+// null and operations//
+const nullTest = null; // absense of value, not that it means 0 but that it has no meaning. good for debugging. can be used if u want to give it another defination.
+const undefinedTest = "test"; // usually when u don't define a variable there is a error,
+console.log(nullTest, undefinedTest);
+//operations --
+const number1 = 72;
+const number2 = 9;
+let add = number1 + number2; //math
+let subtract = number1 + number2; // you cand do multiplication and division using "mult" and "div"
+console.log(add);
+
 //--------------//
 //TRUTH TABLES : ^ = and  v = or
+// null and operations//
